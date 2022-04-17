c846e94835af3e75817ed99a05ac4106e4a77421
Roman <toolatet@gmail.com>
2022-04-17 17:36:46 +0700

    do task 1

---
+++ Example01/Program.cs
@@ -0,0 +1,19 @@
+﻿// прогрмма-проверка палиндрома
+Console.Clear();
+Console.WriteLine("Введите пятизначное число, которое необходимо проверить");
+string number = Convert.ToString(Console.ReadLine());
+if (number.Length != 5)
+{
+    Console.WriteLine($"Указаное число не является пятизначным");
+}
+else
+{
+    if (number[0] == number[4] & number[1] == number[3]) 
+    {
+        Console.WriteLine($"Указаное число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Указаное число - НЕ палиндром"); 
+    }    
+}
