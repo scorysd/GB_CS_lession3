3012ae770d71c3368816d8e02934437bae4724da
Roman <toolatet@gmail.com>
2022-04-17 17:38:04 +0700

    do task 3

---
+++ Example03/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа на вывод таблицы кубов от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int i = 1;
+while (i < number)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+    i++;
+}
+Console.WriteLine($"{Math.Pow(i, 3)}. ");
